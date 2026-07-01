234d74a3432c207b526cca89df7b5f59c301cd4b
awhl666 <awhl666@qq.com>
2026-07-01 15:18:53 +0800

    Add source/neoforge-1.21.1/settings.gradle.kts

---
+++ source/neoforge-1.21.1/settings.gradle.kts
@@ -0,0 +1,3 @@
+pluginManagement { repositories { maven("https://maven.neoforged.net/releases"); gradlePluginPortal(); mavenCentral() } }
+dependencyResolutionManagement { repositoriesMode.set(RepositoriesMode.PREFER_PROJECT); repositories { maven("https://maven.neoforged.net/releases"); mavenCentral() } }
+rootProject.name = "itrp-minimal-capture-neoforge-1.21.1"
